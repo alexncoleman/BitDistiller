8fa06f3ca62b45dddf5dde08e1c0619bcb7d1653
Alex Coleman <111077126+alexncoleman@users.noreply.github.com>
2025-02-20 14:15:16 +0000

    Changing setup.py for T4

--- inference/kernels/setup.py
+++ inference/kernels/setup.py
@@ -2,6 +2,31 @@ from setuptools import find_packages, setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 
 
+# extra_compile_args = {
+#     "cxx": [
+#         "-g", 
+#         "-O3", 
+#         "-fopenmp", 
+#         "-lgomp", 
+#         "-std=c++17",
+#         "-DENABLE_BF16"
+#     ],
+#     "nvcc": [
+#         "-O3", 
+#         "-std=c++17",
+#         "-DENABLE_BF16",  # TODO
+#         "-U__CUDA_NO_HALF_OPERATORS__",
+#         "-U__CUDA_NO_HALF_CONVERSIONS__",
+#         "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+#         "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+#         "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+#         "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+#         "--expt-relaxed-constexpr",
+#         "--expt-extended-lambda",
+#         "--use_fast_math",
+#         "--threads=8"
+#     ],
+# }
 extra_compile_args = {
     "cxx": [
         "-g", 
@@ -14,7 +39,7 @@ extra_compile_args = {
     "nvcc": [
         "-O3", 
         "-std=c++17",
-        "-DENABLE_BF16",  # TODO
+        "-DENABLE_BF16",  
         "-U__CUDA_NO_HALF_OPERATORS__",
         "-U__CUDA_NO_HALF_CONVERSIONS__",
         "-U__CUDA_NO_BFLOAT16_OPERATORS__",
@@ -24,9 +49,11 @@ extra_compile_args = {
         "--expt-relaxed-constexpr",
         "--expt-extended-lambda",
         "--use_fast_math",
-        "--threads=8"
+        "--threads=8",
+        "-arch=sm_75"  # ✅ Only add this here inside "nvcc"
     ],
 }
+
 
 setup(
     name="awq_inference_engine",
